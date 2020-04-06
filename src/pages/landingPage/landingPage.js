2f5d63f618bf1c88cccadc643dd7bfffc59af6f9
LIU <sqfkw1578@gmail.com>
2020-04-06 10:40:17 +0800

    在猶豫要不要全部用module.sass

--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -4,15 +4,18 @@ import Header from '../../components/header'
 import { Link } from 'react-router-dom'
 import './landingPage.sass'
 import MagicCube from './images/magic_cube.svg'
+import styles from './landingPage.module.sass'
 
 function landingPage() {
   return (
-    <>
+    <div className={styles.landingPage}>
       <Header>
         <Link to="/">home</Link>
       </Header>
-      {template.call(this)}
-    </>
+      {template.call(this, {
+        MagicCube
+      })}
+    </div>
   )
 }
 export default landingPage
