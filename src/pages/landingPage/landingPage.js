0e89570976e83b0f652482e36c6cb10f61768b42
LIU <sqfkw1578@gmail.com>
2020-04-06 13:53:56 +0800

    最終決定搭配jsx與一般的sass 來做landingPage

--- src/pages/landingPage/landingPage.js
+++ src/pages/landingPage/landingPage.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import template from './landingPage.pug';
 import Header from '../../components/header'
 import { Link } from 'react-router-dom'
-import './landingPage.sass'
 import MagicCube from './images/magic_cube.svg'
 import styles from './landingPage.module.sass'
 
@@ -12,9 +10,7 @@ function landingPage() {
       <Header>
         <Link to="/">home</Link>
       </Header>
-      {template.call(this, {
-        MagicCube
-      })}
+      <MagicCube className={styles.magicCube}/>
     </div>
   )
 }
