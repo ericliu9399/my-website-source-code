aab49d4873ec12d7046a9c9a4abd3c7d81facf6a
LIU <sqfkw1578@gmail.com>
2020-04-09 09:28:10 +0800

    改進css

--- src/components/nextBtn.js
+++ src/components/nextBtn.js
@@ -1,9 +1,11 @@
 import React from "react"
 import NextIcon from '../images/next.svg'
-import style from './nextBtn.sass'
+import './nextBtn.sass'
 
-export default ({href}) => <a className="next" href={href}>
-  <NextIcon id="n1" />
-  <NextIcon id="n2" />
-  <NextIcon id="n3" />
-</a>
+export default ({ href }) => <div className="nextContainer">
+  <a className="next" href={href}>
+    <NextIcon id="n1" />
+    <NextIcon id="n2" />
+    <NextIcon id="n3" />
+  </a>
+</div>
