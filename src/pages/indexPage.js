1885aada3f75f1fc1007cd71a378f256613f3190
LIU <sqfkw1578@gmail.com>
2020-04-15 11:52:22 +0800

    修改className & id
    snake_case to camelCase

--- src/pages/indexPage.js
+++ src/pages/indexPage.js
@@ -27,7 +27,7 @@ function IndexPage() {
   const links = (
     <>
       <a href="#home">home</a>
-      <a href="#about_me">about me</a>
+      <a href="#aboutMe">about me</a>
       <a href="#projects">projects</a>
       <a href="#contact">contact</a>
       <Link to="/message_board">message board</Link>
@@ -43,22 +43,22 @@ function IndexPage() {
       <main id="indexPage">
         <section id="home">
           <BgImg id="BgImg" />
-          <div className="name_card">
+          <div className="nameCard">
             <p>Hello, i’m</p>
             <h1>Eric Liu{/* Michael Liu | James Liu | William Liu*/}</h1>
             <p>CREATIVE FRONT-END WEB DEVELOPER</p>
           </div>
-          <NextBtn href="#about_me" />
+          <NextBtn href="#aboutMe" />
         </section>
-        <section id="about_me">
-          <h2 className="section_title">About me</h2>
-          <div className="bg_div" />
+        <section id="aboutMe">
+          <h2 className="sectionTitle">About me</h2>
+          <div className="bgDiv" />
           <div className="container">
             <img src={myphoto} alt="" />
             <p>Hi, I'm Eric Liu, a passionate self-taught frontEnd web developer from Taiwan.<br />I tend to make use of modern web technologies to build websites that looks great,<br />feels fantastic, and functions correctly. I am especially focusing on Reactjs.<br /><br />I also do Graphic and UI designing. Since I love both programming and designing,<br />I'm also interested in creating programmatic designs and creative coding projects.</p>
           </div>
-          <h2 className="sub_title">My Skillsets</h2>
-          <div className="skill_container">
+          <h2 className="subTitle">My Skillsets</h2>
+          <div className="skillContainer">
             <a href="#react_link" id="react" ><LogoContainer Logo={<ReactLogo />} /><p>React.js</p></a>
             <a href="#javascript_link" id="javascript" ><LogoContainer Logo={<JavascriptLogo />} /><p>Javascript</p></a>
             <a href="#express_link" id="express" ><LogoContainer Logo={<NodejsLogo />} /><p>Express.js</p></a>
@@ -66,12 +66,12 @@ function IndexPage() {
             <a href="#gulp_link" id="gulp"><LogoContainer Logo={<GulpLogo />} />Gulp</a>
             <a href="#sass_link" id="sass"><LogoContainer Logo={<SassLogo />} />Sass</a>
             <a href="#pug_link" id="pug"><LogoContainer Logo={<PugLogo />} />Pug</a>
-            <a href="#react_router_link" id="react_router"><LogoContainer Logo={<ReactRouterLogo />} />React Router</a>
+            <a href="#reactRouter_link" id="reactRouter"><LogoContainer Logo={<ReactRouterLogo />} />React Router</a>
           </div>
           <NextBtn href="#projects" />
         </section>
         <section id="projects">
-          <h2 className="section_title">Side projects</h2>
+          <h2 className="sectionTitle">Side projects</h2>
           <div className="linkTo">
             <div className="to" id="react_link"></div>
             <div className="to" id="sass_link"></div>
@@ -95,7 +95,7 @@ function IndexPage() {
                   <li>Webpack</li>
                 </ul>
               </li>
-              <li>後端<a href="/">觀看原始碼</a>
+              <li>後端<a href="https://github.com/syyui15926/express-message-board-api/blob/master/index.js">觀看原始碼</a>
                 <ul>
                   <li>express.js</li>
                   <li>Javascript </li>
@@ -109,7 +109,7 @@ function IndexPage() {
             <p>
               這個網頁是用React.js製作的個人網站。
             </p>
-            <a href="/">觀看原始碼</a>
+            <a href="https://github.com/syyui15926/syyui15926.github.io">觀看原始碼</a>
             <ul>
               <li>React.js </li>
               <li>Javascript </li>
@@ -133,7 +133,7 @@ function IndexPage() {
           </div> */}
         </section>
         <section id="contact">
-          <h2 className="section_title">Get In Touch</h2>
+          <h2 className="sectionTitle">Get In Touch</h2>
           <Formspree />
         </section>
       </main>
