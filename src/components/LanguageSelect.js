29e987d1bcad3d1065ec8b7351b9c0e7851a3443
LIU <sqfkw1578@gmail.com>
2020-04-15 16:11:27 +0800

    新增function onClick

--- src/components/LanguageSelect.js
+++ src/components/LanguageSelect.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './LanguageSelect.module.sass'
 import LangLogo from '../images/translate.svg'
-import LogoContainer from './LogoContainer'
 
-function LanguageSelect() {
+function LanguageSelect({ switchLangMethod, language, currentLanguage }) {
+  function onClick() {
+    let index = language.findIndex((lang) => lang == currentLanguage)
+    if (index === language.length - 1)
+      index = 0
+    else
+      index++
+    switchLangMethod(language[index])
+  }
   return (
-    <div className={styles.LanguageSelect}>
+    <div
+      className={styles.LanguageSelect}
+      onClick={onClick}>
       <LangLogo />
     </div>
   )
