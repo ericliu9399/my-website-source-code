09a426a6477536015cada0a5578969b8e0272cda
Eric Liu <sqfkw1578@gmail.com>
2020-04-22 14:39:31 +0800

    移除public

--- public/landingPage/js/all_part.js
+++
@@ -1,3 +0,0 @@
-const mySVG = document.querySelector('svg');
-const viewBox = mySVG.getAttribute("viewBox").split(" ")
-let [minX, minY, width, height] = viewBox
--- public/landingPage/js/hand_bottom.js
+++
@@ -1,3 +0,0 @@
-minY = -20
-mySVG.setAttribute("viewBox",
-  `${minX} ${minY} ${width} ${height}`);
