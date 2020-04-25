93de9ac73450ed8596eaaee9898cddbe5656279d
Eric Liu <sqfkw1578@gmail.com>
2020-04-25 22:07:38 +0800

    移除file-loader

--- webpack/rule/asset.js
+++ webpack/rule/asset.js
@@ -1,12 +1,6 @@
 module.exports = [
   {
-    test: /.*\.(gif|png|jpe?g)$/i,
-    use: {
-      loader: "file-loader"
-    }
-  },
-  {
     test: /\.svg$/,
     use: ['@svgr/webpack'],
-  },
+  }
 ] 
