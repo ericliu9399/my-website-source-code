64acc30588700a5b94a2f6658f032951b7f51e79
Eric Liu <sqfkw1578@gmail.com>
2020-04-25 23:43:38 +0800

    修改rules

---
+++ webpack/rules/asset.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  },
+  {
+    test: /\.(png|jpg|gif)$/i,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: "images/[hash:8].[name].[ext]"
+        },
+      },
+    ],
+  }
+]
+/**
+import myphoto './your path'
+ */
