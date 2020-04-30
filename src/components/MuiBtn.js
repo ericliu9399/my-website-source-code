071f8db1b1aee861818489bd350f5fce9725b413
Eric Liu <sqfkw1578@gmail.com>
2020-04-30 15:31:24 +0800

    新增material ui button

---
+++ src/components/MuiBtn.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  root: {
+    marginLeft: 5,
+    padding: 0,
+    boxShadow: '0 3px 5px 2px rgba(255,255,255,0.3)',//rgba(255, 105, 135, .3)
+    width: "fit-content",
+    height: "fit-content",
+  },
+  label: {
+    padding: 0,
+    margin: 0,
+    width: "fit-content",
+    height: "fit-content",
+  },
+});
+
+
+
+export default function MuiBtn({ children, variant = "contained", color = "primary" }) {
+  const classes = useStyles();
+  console.log(classes)
+  return (
+    <Button
+      variant={variant}
+      color={color}
+      classes={{
+        root: classes.root,
+        label: classes.label
+      }}
+    >
+      {children}
+    </Button>
+  );
+}
