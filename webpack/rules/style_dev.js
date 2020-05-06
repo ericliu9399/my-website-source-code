9e5b4fc17efabbdf742ca125d028dea8f38aaf94
Eric Liu <sqfkw1578@gmail.com>
2020-05-06 14:16:24 +0800

    把rule分離出來

--- webpack/rules/style_dev.js
+++ webpack/rules/style_dev.js
@@ -1,42 +1,91 @@
 const cssModuleRegex = /\.module\.css$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+const cssRegex = /\.css$/;
+const sassRegex = /\.(scss|sass)$/;
 
+const cssRule = {
+  test: cssRegex,
+  exclude: cssModuleRegex,
+  use: [
+    {
+      loader: 'style-loader',
+      options: { attributes: { srcType: 'css' } }
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+      }
+    },
+    {
+      loader: 'postcss-loader'
+    },
+  ]
+}
+const cssModuleRule = {
+  test: cssModuleRegex,
+  use: [
+    {
+      loader: 'style-loader',
+      options: { attributes: { srcType: 'cssModule' } }
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        modules: {
+          mode: 'local',
+          localIdentName: '[path]_[name]__[local]--[hash:base64:5]',
+        },
+        importLoaders: 1
+      }
+    },
+    'postcss-loader'
+  ]
+}
+const sassRule = {
+  test: sassRegex,
+  exclude: sassModuleRegex,
+  use: [
+    {
+      loader: 'style-loader',
+      options: { attributes: { srcType: 'sass' } }
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+      }
+    },
+    {
+      loader: 'postcss-loader'
+    },
+    'sass-loader'
+  ]
+}
+const sassModuleRule = {
+  test: sassModuleRegex,
+  use: [
+    {
+      loader: 'style-loader',
+      options: { attributes: { srcType: 'sassModule' } }
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        modules: {
+          mode: 'local',
+          localIdentName: '[path]_[name]__[local]--[hash:base64:5]',
+        },
+        importLoaders: 1
+      }
+    },
+    'postcss-loader',
+    'sass-loader',
+  ],
+}
 module.exports = [
-  require('./style/css'),
-  {
-    test: cssModuleRegex,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            mode: 'local',
-            localIdentName: '[path]_[name]__[local]--[hash:base64:5]',
-          },
-          importLoaders: 1
-        }
-      },
-      'postcss-loader'
-    ]
-  },
-  require('./style/sass'),
-  {
-    test: sassModuleRegex,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            mode: 'local',
-            localIdentName: '[path]_[name]__[local]--[hash:base64:5]',
-          },
-          importLoaders: 1
-        }
-      },
-      'postcss-loader',
-      'sass-loader',
-    ],
-  },
+  cssRule,
+  cssModuleRule,
+  sassRule,
+  sassModuleRule
 ]
--- webpack/rules/style_prod.js
+++ webpack/rules/style_prod.js
@@ -1,42 +1,79 @@
-const cssModuleRegex = /\.module\.css$/;
-const sassModuleRegex = /\.module\.(scss|sass)$/;
+const cssRegex = /\.css$/
+const cssModuleRegex = /\.module\.css$/
+const sassRegex = /\.(scss|sass)$/
+const sassModuleRegex = /\.module\.(scss|sass)$/
 
+const cssRule = {
+  test: cssRegex,
+  exclude: cssModuleRegex,
+  use: [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+      }
+    },
+    {
+      loader: 'postcss-loader'
+    },
+  ]
+}
+const cssModuleRule = {
+  test: cssModuleRegex,
+  use: [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        modules: {
+          mode: 'local',
+          localIdentName: '[hash:base64:5]',
+        },
+        importLoaders: 1
+      }
+    },
+    'postcss-loader'
+  ]
+}
+const sassRule = {
+  test: sassRegex,
+  exclude: sassModuleRegex,
+  use: [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+      }
+    },
+    {
+      loader: 'postcss-loader'
+    },
+    'sass-loader'
+  ]
+}
+const sassModuleRule = {
+  test: sassModuleRegex,
+  use: [
+    'style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        modules: {
+          mode: 'local',
+          localIdentName: '[hash:base64:5]',
+        },
+        importLoaders: 1
+      }
+    },
+    'postcss-loader',
+    'sass-loader',
+  ],
+}
 module.exports = [
-  require('./style/css'),
-  {
-    test: cssModuleRegex,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            mode: 'local',
-            localIdentName: '[hash:base64:5]',
-          },
-          importLoaders: 1
-        }
-      },
-      'postcss-loader'
-    ]
-  },
-  require('./style/sass'),
-  {
-    test: sassModuleRegex,
-    use: [
-      'style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            mode: 'local',
-            localIdentName: '[hash:base64:5]',
-          },
-          importLoaders: 1
-        }
-      },
-      'postcss-loader',
-      'sass-loader',
-    ],
-  },
+  cssRule,
+  cssModuleRule,
+  sassRule,
+  sassModuleRule
 ]
