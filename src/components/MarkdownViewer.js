fd0742c8b1b8ee44fee2902094825277456fa70a
Eric Liu <sqfkw1578@gmail.com>
2020-05-19 21:05:08 +0800

    改用 readme.md 當內容

---
+++ src/components/MarkdownViewer.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+
+function markdown2html(text) {
+  marked.setOptions({
+    breaks: true,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
+  const renderer = new marked.Renderer();
+  renderer.link = function (href, title, text) {
+    return `<a target="_blank" href="${href}">${text}</a>`;
+  };
+  renderer.table = function (header, body) {
+    return '<table class="table table-striped">' + header + body + '</table>';
+  };
+
+  return marked(text, { renderer: renderer })
+}
+
+/**
+ * @param url {string} './readme.md'
+ */
+function MarkdownViewer({ url }) {
+  const [content, setContent] = useState("")
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.text())
+      .then(text => setContent(markdown2html(text)))
+  }, [url])
+  return <div className="MarkdownViewer" dangerouslySetInnerHTML={{ __html: content }} />
+}
+
+export default MarkdownViewer
---
+++ src/components/Project2.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import SectionTitle from './SectionTitle'
+import MarkdownViewer from './MarkdownViewer'
+
+function Project({ content }) {
+
+  const { title: sectionTitle } = content
+  return (
+    <>
+      <section className="container" id="projects">
+        <SectionTitle title={sectionTitle} />
+        <MarkdownViewer url={"./readme.md"} />
+      </section>
+      <style jsx>{`
+section{
+min-height: 100vh;
+}
+section :global(img){
+display: relative;
+border-radius: 20px;
+width: 50%;
+margin: 40px;
+margin-left: 25%;
+}
+@media all and (max-width: 768px){
+section :global(img){
+width: 100%;
+margin: 10px;
+}}
+.container :global(.MarkdownViewer) :global(h2){
+visibility: hidden;
+}
+.container :global(.MarkdownViewer) :global(a){
+padding: 20px;
+font-size: 30px;
+}
+.container :global(.MarkdownViewer) :global(hr){
+background: white;
+margin: 40px;
+}
+
+`}</style>
+    </>
+  )
+}
+
+export default Project
